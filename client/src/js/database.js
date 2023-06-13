1620e6f304637d752137e55b9511491ac79078e2
mackenzie-wentworth <mwentworth28@gmail.com>
2023-06-13 14:25:10 -0400

    cleaned up TODO comments in webpack.config.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -29,7 +29,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
 
   // Use the .put() method on the store and pass in the content.
-  const request = store.put({ id: 1, jate: content });
+  const request = store.put({ id: 1, value: content });
 
   // Get confirmation of the request.
   const result = await request;
